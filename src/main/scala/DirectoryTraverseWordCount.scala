35f852e59469bc42bc45f211540d0c5a4c7f2517
coconnor <ciaran.o'connor@paddypower.com>
2015-07-02 17:18:57 +0100

    work

--- src/main/scala/DirectoryTraverseWordCount.scala
+++ src/main/scala/DirectoryTraverseWordCount.scala
@@ -1,8 +1,8 @@
 import java.io.File
 
-import akka.actor.{Props, ActorLogging, Actor}
-import akka.event.Logging
+import akka.actor.{Actor, ActorLogging, Props}
 
+import scala.io.Source
 import scala.util.matching.Regex
 
 /*
@@ -24,12 +24,15 @@ class DirectoryTraverseWordCount extends Actor with ActorLogging{
     val files = recursiveListFiles(file,new Regex(".*.txt"))
     for( file <- files) {
       val counter = context.actorOf(Props[CountNumWordsInFile], "CountNumWordsInFile"+Math.random())
+      //      val counter = context.actorOf(Props[CountNumWordsInFile])
+
       counter ! file
     }
   }
 
   import java.io.File
-  import scala.util.matching.Regex
+
+import scala.util.matching.Regex
   def recursiveListFiles(f: File, r: Regex): Array[File] = {
     val these = f.listFiles
     val good = these.filter(f => r.findFirstIn(f.getName).isDefined)
@@ -37,17 +40,49 @@ class DirectoryTraverseWordCount extends Actor with ActorLogging{
   }
 
   def receive = {
-    case _ => log.info("blah")
+    case _ => log.info("unexpected message")
   }
+}
+
+object CountNumWordsInFile {
+  val counter = System.actorOf(Props[CountTotal], "CountTotal")
 }
 
 class CountNumWordsInFile extends Actor with ActorLogging {
 
 
+
   def receive = {
     case file:File =>{
       log.info("received file. Starting to count words in it")
+      counter ! countWordsInFile(file)
     }
     case _ => log.error("unexpected Message")
   }
+
+  def countWordsInFile(file: File) : Int = {
+    val src = Source.fromFile(file)
+
+      (for {
+        line <- src.getLines
+      } yield {
+        val words = line.split("\\s+")
+        words.size
+      }).sum
+
+  }
+}
+
+class CountTotal extends Actor with ActorLogging {
+
+  var total =0
+
+  def receive = {
+    case x:Int => {
+      total = total + x
+      log.info("count = "+ total)
+    }
+    case _ => log.error("unexpected message")
+  }
+
 }
